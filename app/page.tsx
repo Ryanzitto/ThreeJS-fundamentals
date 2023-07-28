fd6649f193a5cb8aa3b5af7e1d911d0d64d2ad4c
Ryan Henrique <ryanhardflip@gmail.com>
2023-07-27 21:53:23 -0300

    feat

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,14 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import Cube from "./components/Cube";
-import Sphere from "./components/Sphere";
-import { OrbitControls } from "@react-three/drei";
-import Text from "./components/TextComponent";
-import AnimatedSphere from "./components/AnimatedSphere";
-import Mage from "./components/Mage";
+import { Experience } from "./components/Experience";
 export default function Home() {
   return (
-    <main className="flex flex-col items-center custom-bg">
-      <Text />
-      <div className="w-full h-full flex-col">
-        <div className="w-full h-[500px] flex">
-          <Canvas className="">
-            <OrbitControls enableZoom={false} autoRotate={true} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <Cube />
-          </Canvas>
-        </div>
-        <div className="w-full h-[500px] flex">
-          <Canvas className="">
-            <OrbitControls enableZoom={false} autoRotate={true} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <Sphere />
-          </Canvas>
-        </div>
-        <div className="w-full h-[500px] flex">
-          <Canvas className="">
-            <OrbitControls enableZoom={false} autoRotate={true} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <AnimatedSphere />
-          </Canvas>
-        </div>
-        <div className="w-full h-[500px] flex ">
-          <Canvas className="">
-            <OrbitControls enableZoom={false} autoRotate={true} />
-            <ambientLight intensity={0.5} />
-            <directionalLight position={[-2, 5, 2]} intensity={1} />
-            <Mage />
-          </Canvas>
-        </div>
+    <main className="flex flex-col items-center h-screen w-screen custom-bg">
+      <div className="w-full h-full flex">
+        <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
+          <color attach="background" args={["#00ffdd"]} />
+          <Experience />
+        </Canvas>
       </div>
     </main>
   );
